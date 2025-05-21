a04da39eb168313d61fcb928c7e731e26386a653
Kundan Goyary <kgoyary824@gmail.com>
2025-05-22 01:38:10 +0530

    Add files via upload

---
+++ dice_duel_arena/script.js
@@ -0,0 +1,21 @@
+function rollDice() {
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+    var image1 = document.querySelector(".img1");
+    var image2 = document.querySelector(".img2");
+
+    image1.setAttribute("src", "./images/dice" + randomNumber1 + ".png");
+    image2.setAttribute("src", "./images/dice" + randomNumber2 + ".png");
+
+    var resultText;
+    if (randomNumber1 > randomNumber2) {
+        resultText = "🔥 Winner is Player 1!";
+    } else if (randomNumber1 === randomNumber2) {
+        resultText = "⚖️ It's a Draw!";
+    } else {
+        resultText = "🔥 Winner is Player 2!";
+    }
+
+    document.querySelector("h1").innerText = resultText;
+}
